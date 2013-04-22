a010d6c14f9b3549ba8c933f691c285e3049af95
Vishwajit Kolathur <aj.ajit@gmail.com>
2013-04-22 23:40:36 +0530

    Update toolchain.sh

--- toolchain.sh
+++ toolchain.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 nasm -f elf -o loader.o loader.s
-/usr/local/cross/bin/x86_64-elf-gcc -o kernel.o -c kernel_test.c -nostartfiles -nodefaultlibs -m32 -nostdlib -ffreestanding 
-/usr/local/cross/bin/x86_64-elf-gcc -o calc.o -c calc1.c -nostartfiles -nodefaultlibs -m32 -nostdlib -ffreestanding
-/usr/local/cross/bin/x86_64-elf-gcc -o page.o -c page.c -nostartfiles -nodefaultlibs -m32 -nostdlib -ffreestanding  
-/usr/local/cross/bin/x86_64-elf-ld -T blink.ld -o kernel.bin loader.o kernel.o calc.o page.o -melf_i386 
-sudo rm /tmp/qemu.log
-qemu-system-x86_64 -kernel kernel.bin -d cpu_reset
+gcc -o kernel.o -c kernel_test.c -nostartfiles -nodefaultlibs -m32 -nostdlib -ffreestanding 
+gcc -o calc.o -c calc1.c -nostartfiles -nodefaultlibs -m32 -nostdlib -ffreestanding
+gcc -o page.o -c page.c -nostartfiles -nodefaultlibs -m32 -nostdlib -ffreestanding  
+ld -T blink.ld -o kernel.bin loader.o kernel.o calc.o page.o -melf_i386 
+
+qemu-system-x86_64 -kernel kernel.bin
